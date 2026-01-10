0f8c5c29472f5173dc152e37f22d277f8a598dd6
so-mb <somtochukwumbuko@gmail.com>
2026-01-10 12:05:47 +0100

    Enhance ESLint configuration for TypeScript by adding test file support and updating global variables; include 'globals' package as a dependency

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,11 +1,12 @@
 import js from '@eslint/js';
 import tseslint from '@typescript-eslint/eslint-plugin';
 import tsparser from '@typescript-eslint/parser';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
   {
-    files: ['src/**/*.{ts,tsx}'],
+    files: ['src/**/*.{ts,tsx}', '!src/**/*.test.{ts,tsx}', '!src/**/*.spec.{ts,tsx}'],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
@@ -16,14 +17,11 @@ export default [
         },
       },
       globals: {
-        // Browser globals
-        window: 'readonly',
-        document: 'readonly',
-        console: 'readonly',
-        // ES2021 globals
-        Promise: 'readonly',
-        Set: 'readonly',
-        Map: 'readonly',
+        ...globals.browser,
+        ...globals.es2021,
+        // React globals
+        React: 'readonly',
+        FC: 'readonly',
       },
     },
     plugins: {
@@ -33,7 +31,41 @@ export default [
       // Disable base rule as it conflicts with TypeScript version
       'no-unused-vars': 'off',
       '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-      'no-console': 'warn',
+      'no-console': 'off',
+      // TypeScript types are not runtime globals, so disable no-undef for type-only usages
+      'no-undef': 'off',
+      '@typescript-eslint/no-undef': 'off',
+    },
+  },
+  {
+    files: ['src/**/*.test.{ts,tsx}', 'src/**/*.spec.{ts,tsx}', 'src/setupTests.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.jest,
+        // React globals
+        React: 'readonly',
+        FC: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'no-undef': 'off',
+      '@typescript-eslint/no-undef': 'off',
     },
   },
   {
